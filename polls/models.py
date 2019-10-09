8f48c75859c43e5163c81d97fcb67cb4ed6acd76
juliano-cunha <julianocunhatrabalho@gmail.com>
2019-10-09 15:18:00 -0300

    Initial commit

---
+++ polls/models.py
@@ -0,0 +1,27 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        """Retorna o nome do campo"""
+        return self.question_text
+
+    def was_published_recently(self):
+        """Registra a alteração comparando o fuso """
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        """Retorna o nome do campo"""
+        return self.choice_text
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\juliano.cunha\PycharmProjects\enquetes\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
