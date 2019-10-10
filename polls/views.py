53a6f6f11a0b5158cbd329ee7cd628b05250fe47
juliano-cunha <julianocunhatrabalho@gmail.com>
2019-10-10 15:25:15 -0300

    Ajuste views polls
    Ajuste nas classes da view polls e criado novo template de resultados

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,6 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 
-
 from .models import Question, Choice
 
 
@@ -21,8 +20,8 @@ def detail(request, question_id):
 
 def results(request, question_id):
     """Detalha o resultado da questão"""
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
@@ -43,5 +42,3 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-
